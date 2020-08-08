3dffd387710c556e87b24b7418002490b71dec2b
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2020-08-08 22:46:59 +0700

    chore: update root config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   // extends: ['@daint2-configs/eslint-config'],
   extends: [require.resolve('./packages/eslint-config/dist')],
   parserOptions: {
-    sourceType: 'module',
     project: ['./packages/*/tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
