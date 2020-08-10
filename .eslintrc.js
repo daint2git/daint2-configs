d476f76ecd0aac51585cc51cdcb9a3a5cfe289fa
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2020-08-10 07:17:46 +0700

    chore: fix root eslint typo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
   root: true,
-  // extends: ['@daint2-configs/eslint-config'],
   extends: [
     './packages/eslint-config/dist/base',
     './packages/eslint-config/dist/jest',
     './packages/eslint-config/dist/typescript',
   ],
   parserOptions: {
-    project: ['tsconfig.json', './packages/*/tsconfig.json'],
+    project: ['./tsconfig.json', './packages/*/tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
 };
